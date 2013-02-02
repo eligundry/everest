1302bd97d422938d9256ad54458794bb1fb1cb91
Eli Gundry <eligundry@gmail.com>
2013-02-02 11:11:46 -0500

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Everest::Application.config.secret_token = 'c7adf74b3d0a0d1244a7bbbe4c759330cfc9049b9631a19cc4c57b198598c1364898789722a4295e2a2f095ba06018233ff0d4753295b43c3a77ecc92b86ecfb'
